cb0c90f6c3a132e82d3369462890a805f2d53319
Thomas Gagyi <thomas.gagyi@gmail.com>
2024-11-14 20:26:52 +0200

    Added further improvements.

--- FindYourRealEstate/Components/Layout/Sections/Hero.razor.cs
+++ FindYourRealEstate/Components/Layout/Sections/Hero.razor.cs
@@ -1,7 +1,13 @@
+using Microsoft.AspNetCore.Components;
+
 namespace FindYourRealEstate.Components.Layout.Sections
 {
     public partial class Hero
     {
+        [Parameter]
+        public required string BackgroundImageUrl { get; set; }
+        [Parameter]
+        public required string HeroWelcomeText { get; set; }
 
     }
 }
